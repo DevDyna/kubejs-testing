6fb73a176aaa1d723258c131098cacbffd8aa207
DevDyna <andrearisorti2003@gmail.com>
2024-03-15 14:30:36 +0100

    magicjs test

--- server_scripts/data.js
+++ server_scripts/data.js
@@ -1,25 +1,79 @@
-BlockEvents.rightClicked('kubejs:core',event=>{
-const {x,y,z} = event.block
+BlockEvents.rightClicked("kubejs:core", (event) => {
+  if (event.hand !== "MAIN_HAND") return;
+  const { block, server ,player , level } = event;
+  const {x,y,z} = block
 
-    if(event.hand == 'MAIN_HAND'){
-    if(event.level.getBlock(x+2,y-1,z).id != 'supplementaries:pedestal'){
-
-
-             event.server.runCommandSilent('particle minecraft:dust 1 0 0 10 '+(x+2)+' '+(y-0.5)+' '+(z)+' 0 0 0 0.1 1 force')
-             event.server.runCommandSilent('particle minecraft:dust 1 1 0 10 '+(x-2)+' '+(y-0.5)+' '+(z)+' 0 0 0 0.1 1 force')
-             event.server.runCommandSilent('particle minecraft:dust 1 0 1 10 '+(x)+' '+(y-0.5)+' '+(z-2)+' 0 0 0 0.1 1 force')
-             event.server.runCommandSilent('particle minecraft:dust 0 1 0 10 '+(x)+' '+(y-0.5)+' '+(z+2)+' 0 0 0 0.1 1 force')
-
-
-
+  const scheme = [
+    [2, -1, 0],
+    [-2, -1, 0],
+    [0, -1, 2],
+    [0, -1, -2],
+  ];
+  let a, b, c;
+  const items = []
+  for ([a, b, c] of scheme) {
+    if (block.offset(a, b, c).id !== "supplementaries:pedestal") {
+      server.runCommandSilent(`particle dust 1 0 0 10 ${x+a} ${y+b + 0.5} ${z+c} 0 0 0 0 1 force`);
+    }else{
+      if(! block.offset(a, b, c).inventory.isEmpty()){
+        items.push({
+          "input": Utils.id(block.offset(a, b, c).inventory.allItems[0].id),
+          "coords":{
+            "x":x+a,
+            "y":y+b,
+            "z":z+c
+          }
+        })
+      }
     }
+  }
 
-    }
+  const recipecollection = [
+    {
+    "input": [
+      'minecraft:stone', 'minecraft:granite',     //I NEED TO FIX THIS BUT NOT TODAY!
+      'minecraft:sand', 'minecraft:dirt'
+    ], 
+    "output": "minecraft:diorite" 
+  },
+  {
+    "input": [
+      'minecraft:cobblestone'
+    ], 
+    "output": "minecraft:gravel"
+  }
+]
+
+  recipecollection.forEach(recipe_id=>{
+    //switch(recipe_id.input.length){
+      //case 1:
+        recipe_id.input.forEach(inrecipe=>{
+          items.forEach(list_in=>{
+            if(list_in.input == inrecipe){
 
 
-    /*event.server.scheduleInTicks(0,a=>{
-        if(event.block.inventory.allItems.isEmpty()){
-            
-        }
-    })*/
-})
+              level.getBlock(list_in.coords.x,list_in.coords.y,list_in.coords.z).inventory.clear(inrecipe)
+              level.getBlock(x,y+1,z).popItemFromFace(recipe_id.output,'up')
+            }
+          })
+        })
+        //break;
+        //case 2:
+        //break;
+        //case 3:
+        //break;
+        //case 4:
+        /*  recipe_id.input.forEach(inrecipe=>{
+            items.forEach(list_in=>{
+              if(list_in.input == inrecipe){
+                level.getBlock(list_in.coords.x,list_in.coords.y,list_in.coords.z).inventory.clear(inrecipe)
+                level.getBlock(x,y+1,z).popItemFromFace(recipe_id.output,'up')
+              }
+            })
+          })
+       */// break;
+        //default:
+        //player.tell('ERROR CASE -> DEFAULT')
+    //}
+  })
+});
--- startup_scripts/star.js
+++ startup_scripts/star.js
@@ -2,3 +2,7 @@ StartupEvents.registry("item", (event) => {
   event.create("minecraft:debug_stick").texture('kubejs:item/star').displayName('Energy Singularity').unstackable()
 
 })
+StartupEvents.registry("item", (event) => {
+  event.create("data:debug_stick").texture('kubejs:item/star').displayName('Energy Singularity').unstackable()
+})
+
