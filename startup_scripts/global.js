aad2fe7ae46695a1b9aa3c361e5bfa3d3b59463e
DevDyna <andrearisorti2003@gmail.com>
2024-05-30 00:13:54 +0200

    random.org

--- startup_scripts/global.js
+++ startup_scripts/global.js
@@ -1,2 +1,8 @@
+//priority 999
+
 Platform.mods.kubejs.name = "Celestial Core";
 const $BooleanProperty = Java.loadClass('net.minecraft.world.level.block.state.properties.BooleanProperty')
+
+function rnd(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
