f3a7a7fdda01c122771cdfb6c6e05cd05919f1e1
DevDyna <andrearisorti2003@gmail.com>
2024-05-30 17:09:17 +0200

    pull from XavBX (latvian discord)

--- startup_scripts/pipe.js
+++ startup_scripts/pipe.js
@@ -1,93 +1,74 @@
+const $IntegerProperty = Java.loadClass(
+  "net.minecraft.world.level.block.state.properties.IntegerProperty"
+);
+ 
+const oppositeDirection = {
+  north: Direction.SOUTH,
+  south: Direction.NORTH,
+  east: Direction.WEST,
+  west: Direction.EAST,
+  down: Direction.UP,
+  up: Direction.DOWN,
+};
+ 
+const directionProperties = {
+  north: BlockProperties.NORTH,
+  south: BlockProperties.SOUTH,
+  east: BlockProperties.EAST,
+  west: BlockProperties.WEST,
+  down: BlockProperties.DOWN,
+  up: BlockProperties.UP,
+};
+ 
 StartupEvents.registry("block", (event) => {
   event
     .create("kubejs:pipe")
-    .property($BooleanProperty.create("north"))
-    .property($BooleanProperty.create("south"))
-    .property($BooleanProperty.create("east"))
-    .property($BooleanProperty.create("west"))
-    .property($BooleanProperty.create("up"))
-    .property($BooleanProperty.create("down"))
+    .property(BlockProperties.NORTH)
+    .property(BlockProperties.SOUTH)
+    .property(BlockProperties.EAST)
+    .property(BlockProperties.WEST)
+    .property(BlockProperties.UP)
+    .property(BlockProperties.DOWN)
+    .property($IntegerProperty.create("type", 0, 1))
     .defaultCutout()
-    .placementState(time=>{
-      time.block.properties = {
-        north: false,
-        south: false,
-        east: false,
-        west: false,
-        up: false,
-        down: false,
+    .defaultState((state) => {
+      // <dev.latvian.mods.kubejs.block.callbacks.BlockStateModifyCallbackJS> state
+      state
+        .set(BlockProperties.NORTH, false)
+        .set(BlockProperties.SOUTH, false)
+        .set(BlockProperties.EAST, false)
+        .set(BlockProperties.WEST, false)
+        .set(BlockProperties.UP, false)
+        .set(BlockProperties.DOWN, false)
+        .set($IntegerProperty.create("type", 0, 1), 0);
+    })
+    .placementState((state) => {
+      // <dev.latvian.mods.kubejs.block.callbacks.BlockStateModifyPlacementCallbackJS> state
+      try {
+        Object.keys(Direction.ALL).forEach((direction) => {
+          let offsetBlock = state.block.offset(direction);
+          if (offsetBlock.id === "kubejs:pipe") {
+            let offsetBlockProp = offsetBlock.properties;
+            offsetBlockProp[oppositeDirection[direction]] = true;
+            offsetBlock.set(offsetBlock.id, offsetBlockProp);
+            state.set(directionProperties[direction], true);
+          }
+        });
+      } catch (error) {
+        console.error(error);
       }
     })
-    .blockEntity((be) => {
-      be.serverTick(1, 0, (tick) => {
-      const { x, y, z } = tick.block;
-      console.log(x + " " + y + " " + z);
-      let position = [
-        [x + 0, y + 0, z - 1],
-        [x + 0, y + 0, z + 1],
-        [x - 1, y + 0, z + 0],
-        [x + 1, y + 0, z + 0],
-        [x + 0, y + 1, z + 0],
-        [x + 0, y - 1, z + 0],
-      ];
-
-      let cardinal = ["north", "south", "east", "west", "up", "down"];
-
-      let prop = {
-        north: false,
-        south: false,
-        east: false,
-        west: false,
-        up: false,
-        down: false,
-      };
-      position.forEach((element, index) => {
-        if (
-          tick.level.getBlock(element[0], element[1], element[2]).id ==
-          "kubejs:pipe"
-        ) {
-          switch (cardinal[index]) {
-            case "north":
-              prop.north = true;
-              break;
-            case "south":
-              prop.south = true;
-              break;
-            case "east":
-              prop.east = true;
-              break;
-            case "west":
-              prop.west = true;
-              break;
-            case "up":
-              prop.up = true;
-              break;
-            case "down":
-              prop.down = true;
-              break;
-          }
-        }
-      });
-
-      tick.block.set(tick.block.id, prop);
-    })
-  })
     .item((item) => {
-      item.modelJson({
-        parent: "kubejs:block/pipe/item_model",
-      });
+      item.modelJson({ parent: "kubejs:block/pipe/item_model" });
     }).blockstateJson = {
     multipart: [
       {
-        when: {
-          north: "false|true",
-          south: "false|true",
-          east: "false|true",
-          west: "false|true",
-          up: "false|true",
-          down: "false|true",
-        },
+        when: { type: "0" },
         apply: { model: "kubejs:block/pipe/core" },
+      },
+      {
+        when: { type: "1" },
+        apply: { model: "kubejs:block/pipe/core1" },
       },
       {
         when: { north: "true" },
@@ -95,11 +76,11 @@ StartupEvents.registry("block", (event) => {
       },
       {
         when: { east: "true" },
-        apply: { model: "kubejs:block/pipe/pipe", y: -90 },
+        apply: { model: "kubejs:block/pipe/pipe", y: 90 },
       },
       {
         when: { west: "true" },
-        apply: { model: "kubejs:block/pipe/pipe", y: 90 },
+        apply: { model: "kubejs:block/pipe/pipe", y: -90 },
       },
       {
         when: { south: "true" },
