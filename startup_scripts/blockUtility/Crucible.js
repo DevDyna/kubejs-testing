c93cad78f38e624209db42c9efb762cfffffb2a6
DevDyna <andrearisorti2003@gmail.com>
2024-06-07 14:38:49 +0200

    fill and empty

--- startup_scripts/blockUtility/Crucible.js
+++ startup_scripts/blockUtility/Crucible.js
@@ -5,46 +5,62 @@ StartupEvents.registry("block", (event) => {
     .defaultState((state) => {
       state.set($BooleanProperty.create("has_water"), false);
     })
+    .rightClick((click) => {
+      const { x, y, z } = click.block;
+      if (click.item.id == "minecraft:water_bucket") {
+        click.player.inventory.extractItem(click.player.inventory.find('minecraft:water_bucket'),1,false)
+        click.player.give('minecraft:bucket')
+        click.level
+          .getBlock(x, y, z)
+          .set("kubejs:crucible", { has_water: true });
+      }
+      if (click.item.id == "minecraft:bucket") {
+        click.player.inventory.extractItem(click.player.inventory.find('minecraft:bucket'),1,false)
+        click.player.give('minecraft:water_bucket')
+        click.level
+          .getBlock(x, y, z)
+          .set("kubejs:crucible", { has_water: false });
+      }
+    })
     .blockEntity((be) => {
       be.serverTick(20, 0, (tick) => {
         const { x, y, z } = tick.block;
-        let area = 1;
-        tick.level.getEntitiesWithin(
-          AABB.of(
-            x - area,
-            y - 1.5,
-            z - area,
-            x + area,
-            y + 1.5,
-            z + area
-          )
-        ).forEach(entity=>{
-          if (entity.type != 'minecraft:item') return
+        if (
+          tick.level.getBlock(x, y - 1, z).hasTag("minecraft:campfires") &&
+          tick.level.getBlock(x, y , z).properties.get("has_water")
+        ) {
+          tick.level
+            .getEntitiesWithin(
+              AABB.of(x - 1, y - 1.5, z - 1, x + 1, y + 1.5, z + 1)
+            )
+            .forEach((entity) => {
+              if (entity.type != "minecraft:item") return;
 
-          if(entity.item=='minecraft:iron_ingot'){
-            entity.setRemoved('unloaded_to_chunk');
-            
-            let rool = entity.item.count > 1 ? entity.item.count : 1
-            for(let i=0;i<rool;i++){
-            tick.block.popItemFromFace(
-              'minecraft:gold_ingot',
-              Direction.UP
-            );
-          }
-          }
-        })
-        tick.level.spawnParticles(
-          "minecraft:smoke",
-          true,
-          x + 0.1 * rnd(1, 9),
-          y + 0.1 * rnd(1, 5),
-          z + 0.1 * rnd(1, 9),
-          0,
-          0.4,
-          0,
-          10,
-          0.1
-        );
+              if (entity.item == "minecraft:iron_ingot") {
+                entity.setRemoved("unloaded_to_chunk");
+
+                let rool = entity.item.count > 1 ? entity.item.count : 1;
+                for (let i = 0; i < rool; i++) {
+                  tick.block.popItemFromFace(
+                    "minecraft:gold_ingot",
+                    Direction.UP
+                  );
+                }
+              }
+            });
+          tick.level.spawnParticles(
+            "minecraft:smoke",
+            true,
+            x + 0.1 * rnd(1, 9),
+            y + 0.1 * rnd(1, 5),
+            z + 0.1 * rnd(1, 9),
+            0,
+            0.4,
+            0,
+            10,
+            0.1
+          );
+        }
       });
     })
     .box(0, 3, 0, 2, 16, 16)
@@ -67,7 +83,9 @@ StartupEvents.registry("block", (event) => {
     }).blockstateJson = {
     variants: {
       "has_water=false": { model: "minecraft:block/cauldron" },
-      "has_water=true": { model: "minecraft:block/water_cauldron_full" },
+      "has_water=true": {
+        model: "minecraft:block/powder_snow_cauldron_level1",
+      },
     },
   };
 });
