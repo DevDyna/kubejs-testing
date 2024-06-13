2d985b90a1eed23715121a01eb6dbcb8b52bb184
DevDyna <andrearisorti2003@gmail.com>
2024-06-13 22:49:46 +0200

    formatted generic

--- startup_scripts/generic.js
+++ startup_scripts/generic.js
@@ -1,11 +1,12 @@
 StartupEvents.registry("block", (event) => {
-    event.create('core').textureAll('minecraft:block/cobblestone').rightClick(a=>{ a.player.swing()})
-})
+  event
+    .create("core")
+    .textureAll("minecraft:block/cobblestone")
+    .rightClick((a) => {
+      a.player.swing();
+    });
+});
 
 StartupEvents.registry("block", (event) => {
-    event.create('gen').blockstateJson = {
-      "variants": {
-          "": { "model": "kubejs:block/gen/model" }
-      }
-  }
-})
+  event.create("gen").model("kubejs:block/gen/model");
+});
