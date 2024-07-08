008c51f349782a29a2e81f7715130211836ba5b0
DevDyna <andrearisorti2003@gmail.com>
2024-07-08 18:23:14 +0200

    fixed vault hunting

--- startup_scripts/blockUtility/Vault.js
+++ startup_scripts/blockUtility/Vault.js
@@ -6,48 +6,43 @@ let loot_items = [
   "minecraft:diamond",
   "minecraft:lapis",
 ];
+
 StartupEvents.registry("block", (event) => {
   event
     .create("vault")
-    .property($BooleanProperty.create("processing"))
-    .property($BooleanProperty.create("unlocked"))
+    .property($BooleanProperty.create("contain_items"))
     .defaultState((state) => {
       state
-        .set($BooleanProperty.create("processing"), false)
-        .set($BooleanProperty.create("unlocked"), true);
+        .set($BooleanProperty.create("contain_items"), true)
     })
     .rightClick((click) => {
       const { item, block, level, server } = click;
-      if (item == "minecraft:emerald" && block.properties.get("unlocked")) {
-        block.set(block.id, {
-          processing: true,
-          unlocked: true,
-        });
-      }
-      if (block.properties.get("processing")) {
-        block.set(block.id, {
-          processing: true,
-          unlocked: false,
-        });
+      const { properties, x, y, z } = block;
+      if (
+        item == "minecraft:emerald" &&
+        properties.get("contain_items") == "true"
+      ) {
         let random = rnd(2, 10);
+
         for (let i = 0; i < random; i++) {
-          console.log(i);
           server.scheduleInTicks((i + 1) * 10, () => {
-            let { x, y, z } = block;
-            let compost = level.createEntity("item");
-            compost.x = x + 0.5;
-            compost.y = y + 1.5;
-            compost.z = z + 0.5;
-            compost.item = loot_items[rnd(0, 5)];
-            compost.item.count = rnd(1, 16);
-            console.log(compost);
-            compost.spawn();
+            block.popItemFromFace(loot_items[rnd(0, 5)], "up");
+            level.spawnParticles(
+              "minecraft:sonic_boom",
+              true,
+              x + 0.5,
+              y + 1.5,
+              z + 0.5,
+              0,
+              0,
+              0,
+              1,
+              0.1
+            );
           });
         }
-        block.set(block.id, {
-          processing: false,
-          unlocked: false,
-        });
+      } else {
+        return;
       }
     })
     .item((item) => {
@@ -55,11 +50,9 @@ StartupEvents.registry("block", (event) => {
         parent: "kubejs:block/dynamo/off",
       });
     }).blockstateJson = {
-    variants: {
-      //"active=true": { "model": "kubejs:block/"+id_block+"/on" },
-      //"active=false": { "model": "kubejs:block/"+id_block+"/off" }
-      "processing=true": { model: "kubejs:block/dynamo/on" },
-      "processing=false": { model: "kubejs:block/dynamo/off" },
-    },
-  };
+      variants: {
+        "contain_items=true": { model: "kubejs:block/dynamo/on" },
+        "contain_items=false": { model: "kubejs:block/dynamo/off" },
+      },
+    };
 });
